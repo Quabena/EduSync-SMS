54f39ac1b7779ea806a792b7edb398a520704e3e
Ev_Ans <ivanz.corbie@gmail.com>
2025-11-20 01:55:11 +0000

    Update

--- config.py
+++ config.py
@@ -17,7 +17,6 @@ BASE_DIR.mkdir(parents=True, exist_ok=True)
 class Config:
     SECRET_KEY = (
         os.environ.get("SECRET_KEY")
-        or "43cba8188f13db93ab9b2f57d569b7c3b6ca59a6b2fcb623c6988be6e527a9b3"
     )
 
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{BASE_DIR / 'edusync.db'}"
